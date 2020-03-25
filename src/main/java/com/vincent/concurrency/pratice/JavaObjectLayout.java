9ff5b9e797e65dba6f2863ba2925a5bc128670fd
Mac <315744068@qq.com>
2020-03-25 21:11:37 +0800

    打印对象的布局

---
+++ src/main/java/com/vincent/concurrency/pratice/JavaObjectLayout.java
@@ -0,0 +1,16 @@
+package com.vincent.concurrency.pratice;
+
+import org.openjdk.jol.info.ClassLayout;
+
+public class JavaObjectLayout {
+
+    public static void main(String[] args) {
+
+        JavaObjectLayout object = new JavaObjectLayout();
+
+//        System.out.println(ClassLayout.parseInstance(object).toPrintable());
+        synchronized (object){
+            System.out.println(ClassLayout.parseInstance(object).toPrintable());
+        }
+    }
+}
